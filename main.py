c8d8003ae505354f9f12a6490f05c432e9fba059
nfonteyne <nathan.fonteyne59@gmail.com>
2023-01-07 21:42:28 +0100

    charts ps5 switch trends

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from flask import render_template, redirect, url_for, Flask
+import pytrends
+import pandas as pd
+from pytrends.request import TrendReq
+from datetime import datetime
+
+# execute the TrendReq method by passing the host language (hl) and timezone (tz) parameters
+
 
 app = Flask(__name__)
 
-@app.route("/home")
-def home():
-    return render_template("main.html")
-@app.route("/<name>")
-def user(name):
-    return f"Hello-- {name}!"
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
+
+@app.route("/")
+def trend():
+
+    pytrends = TrendReq(hl='en-US', tz=360)
+    pytrends.build_payload(kw_list=['switch','ps5'])
+    interest_over_time_df = pytrends.interest_over_time()
+    switch = interest_over_time_df['switch'].values.tolist()
+    ps5 = interest_over_time_df['ps5'].values.tolist()
+    dates = [datetime.fromtimestamp(int(date/1e9)).date().isoformat() for date in interest_over_time_df.index.values.tolist()]
+
+    return render_template('main.html', labels=dates, myswitch=switch, myps5=ps5)
+
+if __name__ == "__main__":
+    app.run()
