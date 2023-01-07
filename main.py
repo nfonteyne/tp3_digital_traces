d48742e67ecf34160f4e352c498912e4f23de5fe
nfonteyne <nathan.fonteyne59@gmail.com>
2023-01-07 22:33:56 +0100

    decorator and counting functions

--- main.py
+++ main.py
@@ -3,16 +3,47 @@ import pytrends
 import pandas as pd
 from pytrends.request import TrendReq
 from datetime import datetime
-
-# execute the TrendReq method by passing the host language (hl) and timezone (tz) parameters
-
+from functools import wraps
+import time
+from collections import Counter
 
 app = Flask(__name__)
 
 
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        return result
+    return timeit_wrapper
+
+
+@timeit
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
+
+@timeit
+def count_with_counter_function(str):
+    alltext=str.split()
+    counter = Counter(alltext)
+    return counter
+
+
 @app.route("/")
 def trend():
-
     pytrends = TrendReq(hl='en-US', tz=360)
     pytrends.build_payload(kw_list=['switch','ps5'])
     interest_over_time_df = pytrends.interest_over_time()
@@ -22,5 +53,11 @@ def trend():
 
     return render_template('main.html', labels=dates, myswitch=switch, myps5=ps5)
 
+
 if __name__ == "__main__":
+    with open('t8shakespeare.txt', 'r') as file:
+        data = file.read().replace('\n', '')
+
+    #word_count(data)
+    print(count_with_counter_function(data))
     app.run()
